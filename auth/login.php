0be85d14b67b39ad085d02f1b738d32845562e6e
RievalAzza <rievalazza@gmail.com>
2025-07-31 07:51:40 +0700

    1

--- auth/login.php
+++ auth/login.php
@@ -8,8 +8,8 @@
 <body class="bg-gray-100 flex items-center justify-center h-screen">
     <form action="proses_login.php" method="POST" class="bg-white p-6 rounded shadow w-80">
         <h2 class="text-xl font-bold mb-4">Login Admin</h2>
-        <input type="text" name="username" placeholder="Username" class="w-full mb-3 p-2 border rounded">
-        <input type="password" name="password" placeholder="Password" class="w-full mb-3 p-2 border rounded">
+        <input type="text" name="username" placeholder="Username" class="w-full mb-3 p-2 border rounded" required>
+        <input type="password" name="password" placeholder="Password" class="w-full mb-3 p-2 border rounded" required>
         <button type="submit" class="bg-blue-500 text-white w-full py-2 rounded">Login</button>
     </form>
 </body>
